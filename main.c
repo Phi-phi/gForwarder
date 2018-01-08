85a5d0cf671fbd64db5ca9751979e04146796123
Kazuaki Masuda <kame.kazu.0213.masumerry.0112@gmail.com>
2018-01-09 02:06:32 +0900

    separate cur

--- main.c
+++ main.c
@@ -13,14 +13,22 @@
 
 struct nm_desc *nm_desc;
 
+void printHex(char* buf, size_t len) {
+  int i;
+  for(i = 0; i < len; ++i) {
+    printf("%02X", ((unsigned char*)buf)[i]);
+  }
+  printf("\n");
+}
+
 void swapto(int to_hostring, struct netmap_slot *rxslot) {
   struct netmap_ring *txring;
-  int i, first, last, sent = 0;
+  int i, first, last, sent = 0;;
   uint32_t t, cur;
 
   if (to_hostring) {
     fprintf(stderr, "NIC to HOST\n");
-    first = last = nm_desc->last_tx_ring; // txring is hostring.
+    first = last = nm_desc->last_tx_ring;
   } else {
     fprintf(stderr, "HOST to NIC\n");
     first = nm_desc->first_tx_ring;
@@ -29,28 +37,28 @@ void swapto(int to_hostring, struct netmap_slot *rxslot) {
 
   for (i = first; i <= last && !sent; ++i) {
     txring = NETMAP_TXRING(nm_desc->nifp, i);
+    while(!nm_ring_empty(txring)) {
+      cur = txring->cur;
 
-    if (nm_ring_empty(txring)) {
-      continue;
+      if (txring->slot[cur].flags & NS_BUF_CHANGED) {
+        // this slot has already changed
+        txring->head = txring->cur = nm_ring_next(txring, cur);
+        continue;
+      }
+      t = txring->slot[cur].buf_idx;
+      txring->slot[cur].buf_idx = rxslot->buf_idx;
+      rxslot->buf_idx = t;
+
+      txring->slot[cur].len = rxslot->len;
+
+      txring->slot[cur].flags |= NS_BUF_CHANGED;
+      rxslot->flags |= NS_BUF_CHANGED;
+
+      sent = 1;
+      txring->head = txring->cur = nm_ring_next(txring, cur);
+
+      break;
     }
-
-    cur = txring->cur;
-
-    if (txring->slot[cur].flags & NS_BUF_CHANGED) {
-      continue;
-    }
-    t = txring->slot[cur].buf_idx;
-    txring->slot[cur].buf_idx = rxslot->buf_idx;
-    rxslot->buf_idx = t;
-
-    txring->slot[cur].len = rxslot->len;
-
-    txring->slot[cur].flags |= NS_BUF_CHANGED;
-    rxslot->flags |= NS_BUF_CHANGED;
-
-    sent = 1;
-
-    txring->head = txring->cur = nm_ring_next(txring, cur);
   }
 }
 
@@ -73,7 +81,7 @@ void change_ip_addr(char* pkt, const char* new_src, const char* new_dst, int dst
 }
 
 int main(int argc, char* argv[]) {
-  unsigned int cur, i, t_i, is_hostring;
+  unsigned int r_cur, t_cur, i, t_i, is_hostring;
   int sent = 0, pktsizelen;
   char *buf, *payload, *tbuf;
   struct netmap_ring *rxring, *txring;
@@ -98,9 +106,9 @@ int main(int argc, char* argv[]) {
       if(nm_ring_empty(rxring))
         continue;
 
-      cur = rxring->cur;
-      buf = NETMAP_BUF(rxring, rxring->slot[cur].buf_idx);
-      pktsizelen = rxring->slot[cur].len;
+      r_cur = rxring->cur;
+      buf = NETMAP_BUF(rxring, rxring->slot[r_cur].buf_idx);
+      pktsizelen = rxring->slot[r_cur].len;
 
       ether = (struct ether_header *)buf;
 
@@ -108,18 +116,19 @@ int main(int argc, char* argv[]) {
         printf("This is ARP.\n");
         arp = (struct ether_arp *)(buf + sizeof(struct ether_header));
 
-        swapto(!is_hostring, &rxring->slot[cur]);
-        rxring->head = rxring->cur = nm_ring_next(rxring, cur);
+        swapto(!is_hostring, &rxring->slot[r_cur]);
+        rxring->head = rxring->cur = nm_ring_next(rxring, r_cur);
         continue;
       }
       ip = (struct ip *)(buf + sizeof(struct ether_header));
       payload = (char *)ip + (ip->ip_hl<<2);
 
       if (ip->ip_p == IPPROTO_UDP) {
+        printHex(buf, pktsizelen);
         sent = 0;
         udp = (struct udphdr *)payload;
         change_ip_addr(buf, "10.2.2.2", "10.2.2.3", 11233);
-        rxring->slot[cur].flags |= NS_BUF_CHANGED;
+        rxring->slot[r_cur].flags |= NS_BUF_CHANGED;
 
         for (t_i = nm_desc->first_tx_ring; t_i < nm_desc->last_tx_ring && !sent; ++t_i) {
           txring = NETMAP_TXRING(nm_desc->nifp, t_i);
@@ -127,21 +136,26 @@ int main(int argc, char* argv[]) {
           if (nm_ring_empty(txring))
             continue;
 
-          cur = txring->cur;
+          t_cur = txring->cur;
 
-          txring->slot[cur].buf_idx = rxring->slot[cur].buf_idx;
-          txring->slot[cur].len = pktsizelen;
-          txring->slot[cur].flags |= NS_BUF_CHANGED;
+          txring->slot[t_cur].buf_idx = rxring->slot[r_cur].buf_idx;
+          txring->slot[t_cur].len = pktsizelen;
+          txring->slot[t_cur].flags |= NS_BUF_CHANGED;
 
-          txring->head = txring->cur = nm_ring_next(txring, cur);
+          txring->head = txring->cur = nm_ring_next(txring, t_cur);
           sent = 1;
           printf("ok.\n");
         }
       } else {
-        swapto(!is_hostring, &rxring->slot[cur]);
+        if (ntohs(ether->ether_type) > 0) {
+          swapto(!is_hostring, &rxring->slot[r_cur]);
+        } else {
+          printf("blank pkt\n");
+          printHex(buf, pktsizelen);
+        }
       }
 
-      rxring->head = rxring->cur = nm_ring_next(rxring, cur);
+      rxring->head = rxring->cur = nm_ring_next(rxring, r_cur);
     }
   }
 
